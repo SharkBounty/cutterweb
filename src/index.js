f3adf2371add6c3a90b36ae1cda5d82b436349ba
SharkBounty <mateushexa@gmail.com>
2025-04-27 15:12:37 -0300

    vercelbug

--- src/index.js
+++ src/index.js
@@ -15,6 +15,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
-module.exports = app; // Export the Express app
+// module.exports = app; // Export the Express app
 
 reportWebVitals();
